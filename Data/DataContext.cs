0caf96140dfade6f5579d92256e258d3f120b72c
alf <alfmohenu@outlook.com>
2018-07-25 00:21:44 +0300

    Working skeleton Value model added. Sqlite added.

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Waless.API.Models;
+
+namespace Waless.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Models/Value.cs
@@ -0,0 +1,8 @@
+namespace Waless.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
